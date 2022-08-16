7dfa17e52b0ba7f492c00f290d62eecbac2206fe
Rubens <rubens_olv@hotmail.com>
2022-08-16 09:05:02 -0600

    update script

--- js/scripts.js
+++ js/scripts.js
@@ -207,7 +207,8 @@ $(document).ready(function () {
     $('#add-to-cal').html(myCalendar);
 
 
-    /********************** RSVP **********************/
+    /********************** RSVP 271117 **********************/
+
     $('#rsvp-form').on('submit', function (e) {
         e.preventDefault();
         var data = $(this).serialize();
@@ -218,7 +219,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbz_IViefJxk6mKNeExKYpMtPa3HF06UHg4WtzJKt6Hd-yVMWfMQyGDir25deFk77-fm/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
